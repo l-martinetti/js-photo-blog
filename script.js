b8b6aaff7e0af2d6fc1be0e693707dc296e39858
l-martinetti <luca.martinetti@outlook.it>
2025-03-16 17:09:14 +0100

    fix; add new API for cards print

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const closeButton = overlay.querySelector("button");
 axios.get(endpoint)
     .then(res => {
         // Stamp cards
-        res.data.forEach(post => post.id < 6 ? newCard(post) : null);
+        res.data.forEach(post => post.id <= 8 ? newCard(post) : null);
     })
     .catch(error => {
         console.log("Errore: ", error);
@@ -32,14 +32,14 @@ closeButton.addEventListener("click", () => {
 
 // Functions
 function newCard(post) {
-    const { title, photoUrl } = post;
+    const { title, photoUrl, description } = post;
     cardContainer.innerHTML +=
         `<div class="card" style="width: 18rem;">
-  <img src=${photoUrl} class="card-img-top" alt${title}>
-  <div class="card-body">
+  <img src=${photoUrl} class="card-img-top mt-2" alt${title} style="width: 100%; max-height: 200px; object-fit: cover;">
+  <div class="card-body d-flex flex-column align-items-center">
     <h5 class="card-title">${title}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button class="btn btn-primary">Go somewhere</button>
+    <p class="card-text">${description}</p>
+    <button class="btn btn-primary mt-2">Scopri di pi&ugrave</button>
   </div>
 </div>`;
 }
